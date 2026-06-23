3c1d19ee74e27d3eb859ee7f41361053feb083d1
ashkingunity <ashkingunity@gmail.com>
2026-06-23 14:06:16 +0530

    Section Main Menu - Common Button Base

---
+++ Source/FrontendUI/Private/Widgets/Components/FrontendCommonButtonBase.cpp
@@ -0,0 +1,20 @@
+// Ashish Dandge All Rights Reserved
+
+
+#include "Widgets/Components/FrontendCommonButtonBase.h"
+#include "CommonTextBlock.h"
+
+void UFrontendCommonButtonBase::SetButtonText(FText InText)
+{
+	if (CommonTextBlock_ButtonText && !InText.IsEmpty())
+	{
+		CommonTextBlock_ButtonText->SetText(bUseUpperCaseForButtonText ? InText.ToUpper() : InText);
+	}
+}
+
+void UFrontendCommonButtonBase::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	SetButtonText(ButtonDisplayText);
+}
